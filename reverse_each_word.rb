3b20edc3f33faaaeb612fa5047f6543c2413f2f8
kitte anderson <kitteanderson@gmail.com>
2018-12-16 19:41:43 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+
+
+def reverse_each_word(array)
+  reversed_array = array.split(" ")
+  reversed_array.collect {|x| x.reverse!}
+  reversed_array.join(" ")
+  #array.split(" ").collect {|x| x.reverse}.join(" ")
+end
